a2bb1fa269e0d623cb3eee636327f91a3df0b314
nabecchi <1919kimonolove@gmail.com>
2021-10-23 02:58:29 +0000

    validation

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -52,6 +52,11 @@ class MessagesController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+
         // メッセージを作成
         $message = new Message;
         $message->content = $request->content;
@@ -107,6 +112,11 @@ class MessagesController extends Controller
     // putまたはpatchでmessages/(任意のid)にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+
         // idの値でメッセージを検索して取得
         $message = Message::findOrFail($id);
         // メッセージを更新
